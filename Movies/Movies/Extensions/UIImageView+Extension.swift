eca2c1e8b032784c687d32d17ed6b17535aca0bc
ilentiy <i.brykov9@bk.ru>
2023-01-09 22:22:46 +0300

    fix

--- Movies/Movies/Extensions/UIImageView+Extension.swift
+++ Movies/Movies/Extensions/UIImageView+Extension.swift
@@ -4,15 +4,18 @@
 import Foundation
 import UIKit
 
+/// Расширение для получения фото друга
 extension UIImageView {
-    func load(url: URL) {
-        DispatchQueue.global().async { [weak self] in
-            if let data = try? Data(contentsOf: url) {
-                if let image = UIImage(data: data) {
-                    DispatchQueue.main.async {
-                        self?.image = image
-                    }
-                }
+    func load(url: String) {
+        guard let url = URL(string: url) else { return }
+        DispatchQueue.global().async {
+            guard let data = try? Data(contentsOf: url),
+                  let image = UIImage(data: data)
+            else {
+                return
+            }
+            DispatchQueue.main.async {
+                self.image = image
             }
         }
     }
